f356d18a7eb078c2558d7a06723242ccf5c82699
Ramarti <raul@storyprotocol.xyz>
2023-10-18 22:10:05 -0300

    Update uploader task to work with release v0.1.0 (#134)
    * uploader task for assets and relationships
    
    * adapted batch uploader
    
    * Update README.md
    
    * updated readme instructions
    
    ---------
    
    Co-authored-by: Brent Ju <brentju03@gmail.com>
    Co-authored-by: Raul <raul@proofofcode.dev>
    Co-authored-by: brentju <68607591+brentju@users.noreply.github.com>

--- script/hardhat/loadDeployment.js
+++ script/hardhat/loadDeployment.js
@@ -15,11 +15,21 @@ async function main(hre) {
         });
     }
     const contracts = {}
-    contracts.FranchiseRegistry = await ethers.getContractFactory("FranchiseRegistry");
-    contracts.franchiseRegistry = await contracts.FranchiseRegistry.attach(deployment.main["FranchiseRegistry-Proxy"]);
+    contracts.IPOrgController = await ethers.getContractFactory("IPOrgController-Proxy");
+    contracts.ipOrgController = await contracts.IPOrgController.attach(deployment.main["IPOrgController-Proxy"]);
     contracts.IPAssetsRegistry = await ethers.getContractFactory("IPAssetRegistry");
-    contracts.ProtocolRelationshipModule = await ethers.getContractFactory("ProtocolRelationshipModule");
-    contracts.protocolRelationshipModule = await contracts.ProtocolRelationshipModule.attach(deployment.main["ProtocolRelationshipModule-Proxy"]);
+    contracts.ipAssetsRegistry = await contracts.IPAssetsRegistry.attach(deployment.main["IPAssetRegistry"]);
+    contracts.RegistrationModule = await ethers.getContractFactory("RegistrationModule");
+    contracts.registrationModule = await contracts.RegistrationModule.attach(deployment.main["RegistrationModule-Proxy"]);
+    contracts.LicenseRegistry = await ethers.getContractFactory("LicenseRegistry");
+    contracts.licenseRegistry = await contracts.LicenseRegistry.attach(deployment.main["LicenseRegistry"]);
+    contracts.LicensingModule = await ethers.getContractFactory("LicensingModule");
+    contracts.licensingModule = await contracts.LicensingModule.attach(deployment.main["LicensingModule"]);
+    contracts.RelationshipModule = await ethers.getContractFactory("RelationshipModule");
+    contracts.relationshipModule = await contracts.RelationshipModule.attach(deployment.main["RelationshipModule"]);
+    contracts.StoryProtocol = await ethers.getContractFactory("StoryProtocol");
+    contracts.storyProtocol = await contracts.StoryProtocol.attach(deployment.main["StoryProtocol"]);
+    
     return { chainId, contracts, deployment };
 }
 
